3ac5ef792468d3c79d9bfad73fbb8c0f867300ca
Kytusla908 <jordiren99@gmail.com>
2025-12-02 17:57:30 +0100

    fixed names of files

---
+++ 02_model_training.R
@@ -0,0 +1,36 @@
+library(caret)
+
+# Load data and labels ========================
+TCGA.BEAT_data <- read.table("OutputTables/NormalizedCounts_TCGA-BEAT_filterByExpr_vst_corrFiltered.txt",
+                             header=T, sep="\t")
+TCGA.BEAT_labels <- read.table("InputTables/TCGA_BEAT_labels.txt", header=T, sep="\t")
+
+# Transpose to prepare for input
+TCGA.BEAT_data <- t(TCGA.BEAT_data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
