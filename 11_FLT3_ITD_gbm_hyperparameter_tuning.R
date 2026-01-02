fabdc2a23c80826a3f600a65d14c6638505cbfad
kytusla908 <jordiren99@gmail.com>
2026-01-02 14:16:29 +0100

    Assess FLT3_ITD gbm model performance and varImportance

--- 11_FLT3_ITD_gbm_hyperparameter_tuning.R
+++ 11_FLT3_ITD_gbm_hyperparameter_tuning.R
@@ -2,6 +2,7 @@ source("00_helper_functions.R")
 library(tidyverse)
 library(caret)
 library(doParallel)
+library(gbm)
 
 
 # Load data =================================
@@ -87,8 +88,8 @@ print(Sys.time()-start_time)
 
 # saveRDS(gbm_tuned, "OutputTables/FLT3_ITD_tuned_gbm_vst_min_max_smaller.rds")
 # saveRDS(gbm_tuned, "OutputTables/FLT3_ITD_tuned_gbm_vst_min_max_not_so_small.rds")
-# gbm_tuned_small <- readRDS("OutputTables/FLT3_ITD_tuned_gbm_vst_min_max_smaller.rds")
-# gbm_tuned_big <- readRDS("OutputTables/FLT3_ITD_tuned_gbm_vst_min_max_not_so_small.rds")
+gbm_tuned_small <- readRDS("OutputTables/FLT3_ITD_tuned_gbm_vst_min_max_smaller.rds")
+gbm_tuned_big <- readRDS("OutputTables/FLT3_ITD_tuned_gbm_cv_vst_min_max_not_so_small.rds")
 
 
 # Obtain top 10 best performing models ====================
@@ -118,36 +119,88 @@ top10_gbm_small <- readRDS("OutputTables/FLT3_ITD_top10_gbm_models_smaller.rds")
 top10_gbm_big <- readRDS("OutputTables/FLT3_ITD_top10_gbm_models_not_so_small.rds")
 
 
+# Join all models tables ==================================
+gbm_all_performance <- rbind(gbm_tuned_small[["results"]], gbm_tuned_big[["results"]])
+gbm_all_performance <- gbm_all_performance %>% arrange(desc(Sens))
+gbm_all_performance %>% head(10) %>%
+  select(n.trees, interaction.depth, shrinkage, n.minobsinnode)
+
+
 # Check performance ====================================
+# Test data
 performance <- get_performance(gbm_tuned_small, x_test_vst_min_max, y_test,
-                               classes = c("FLT3.Mutant", "FLT3.WT"))
+                               classes = c("FLT3_ITD.Mutant", "FLT3_ITD.WT"),
+                               plot_title = "gbm model with highest Sensitivity (on test data)",
+                               plot_subtitle=paste0("n.trees=", gbm_tuned_small[["bestTune"]][["n.trees"]],
+                                                    " interaction.depth=", gbm_tuned_small[["bestTune"]][["interaction.depth"]],
+                                                    " shrinkage=", gbm_tuned_small[["bestTune"]][["shrinkage"]],
+                                                    " n.minobsinnode=", gbm_tuned_small[["bestTune"]][["n.minobsinnode"]]))
+# ggsave("plots/FLT3_ITD/ROC_test_tuned_gbm_vst_min_max_3000.png", device = "png", width = 15, height = 15,
+#        units = "cm", pointsize = 10, dpi = 500)
 # write_confusion_matrix(performance$confusion_matrix,
-#                        "OutputTables/FLT3_validation_test_gbm_cm.txt")
+#                        "OutputTables/FLT3_ITD_validation_test_gbm_cm.txt")
+
+# Fischer data
 performance <- get_performance(gbm_tuned_small, Fischer_vst_min_max, Fischer_labels,
-                               classes = c("FLT3.Mutant", "FLT3.WT"))
+                               classes = c("FLT3_ITD.Mutant", "FLT3_ITD.WT"),
+                               plot_title = "gbm model with highest Sensitivity (on Fischer data)",
+                               plot_subtitle=paste0("n.trees=", gbm_tuned_small[["bestTune"]][["n.trees"]],
+                                                    " interaction.depth=", gbm_tuned_small[["bestTune"]][["interaction.depth"]],
+                                                    " shrinkage=", gbm_tuned_small[["bestTune"]][["shrinkage"]],
+                                                    " n.minobsinnode=", gbm_tuned_small[["bestTune"]][["n.minobsinnode"]]))
+# ggsave("plots/FLT3_ITD/ROC_Fischer_tuned_gbm_vst_min_max_3000.png", device = "png", width = 15, height = 15,
+#        units = "cm", pointsize = 10, dpi = 500)
 # write_confusion_matrix(performance$confusion_matrix,
-#                        "OutputTables/FLT3_validation_Fischer_gbm_cm.txt")
+#                        "OutputTables/FLT3_ITD_validation_Fischer_gbm_cm.txt")
 
 
-# Top 10 best performing gbms ========================
-fischer_top5_roc <- vector("list", 5)
-fischer_top5_auc <- c()
-for (i in 1:5) {
+# Top 5 best performing gbms (test data) ========================
+test_performance_grid <- gbm_all_performance %>% head(5) %>%
+  select(n.trees, interaction.depth, shrinkage, n.minobsinnode)
+test_performance_grid$Accuracy <- NA_real_
+test_performance_grid$Sens <- NA_real_
+test_performance_grid$Spec <- NA_real_
+test_performance_grid$Prec <- NA_real_
+test_performance_grid$Kappa <- NA_real_
+test_performance_grid$AUC <- NA_real_
+test_performance_top5_roc <- vector("list", nrow(test_performance_grid))
+
+for (i in seq_len(nrow(test_performance_grid))) {
+  
+  cat("Assessing performance of model", i, "of", nrow(test_performance_grid), "\n")
+  
   model <- top10_gbm_small[[i]]
-  performance_df <- get_performance(top10_gbm_small[[i]], Fischer_vst_min_max, Fischer_labels,
+  
+  performance_df <- get_performance(top10_gbm_small[[i]], x_test_vst_min_max, y_test,
                                     classes = c("FLT3_ITD.Mutant", "FLT3_ITD.WT"))
-  fischer_top5_auc <- append(fischer_top5_auc, performance_df$auc)
-  fischer_top5_roc[[i]] <- performance_df$roc_df
+  
+  # Extract metrics from confusion matrix
+  cm <- performance_df$confusion_matrix
+  test_performance_grid$Sens[i] <- cm$byClass["Sensitivity"]
+  test_performance_grid$Spec[i] <- cm$byClass["Specificity"]
+  test_performance_grid$Prec[i] <- cm$byClass["Pos Pred Value"]
+  test_performance_grid$Accuracy[i] <- cm$overall["Accuracy"]
+  test_performance_grid$Kappa[i] <- cm$overall["Kappa"]
+  test_performance_grid$AUC[i] <- performance_df$auc
+  
+  test_performance_top5_roc[[i]] <- performance_df$roc_df
 }
 
+names(test_performance_top5_roc) <- paste0("model ", seq_along(test_performance_top5_roc))
+test_performance_grid <- cbind(test_performance_grid, data.frame(model=paste0("model ", 1:5)))
+# write.table(test_performance_grid, "OutputTables/FLT3_ITD_tuned_top5_gbm_models_validation_test_performance_table.txt",
+#             row.names = T, col.names = T, quote = F, sep = "\t")
+# saveRDS(test_performance_top5_roc, "OutputTables/FLT3_ITD_tuned_top5_gbm_models_validation_test_performance_roc.rds")
+
+# Plot test ROC curves 
 roc_5_df <- bind_rows(lapply(1:5, function(i) {
-  df <- fischer_top5_roc[[i]]
+  df <- test_performance_top5_roc[[i]]
   df$model <- paste0("Model ", i)
   df
 }))
 auc_labels <- paste0("Model ", 1:5,
                      " (AUC = ",
-                     round(fischer_top5_auc[1:5], 3), ")")
+                     round(test_performance_grid$AUC[1:5], 3), ")")
 roc_5_df$model <- factor(roc_5_df$model,
                          levels = paste0("Model ", 1:5),
                          labels = auc_labels)
@@ -155,40 +208,97 @@ ggplot(roc_5_df, aes(x = fpr, y = tpr, color = model)) +
   geom_line(size = 1.2) +
   geom_abline(intercept = 0, slope = 1,
               linetype = "dashed", color = "gray") +
-  labs(title = "ROC Curves for Top 5 gbm models for FLT3_ITD mutation on Fischer data",
+  labs(title = "ROC Curves for Top 5 gbm models on test data",
        x = "False Positive Rate", y = "True Positive Rate",
        color = "Model") +
   theme_minimal() +
   theme(panel.background = element_rect(fill = "white", color = NA),
         plot.background  = element_rect(fill = "white", color = NA))
-# ggsave("plots/FLT3/ROC_Fischer_top5_gbm_vst_min_max_3000.png", device = "png", width = 18, height = 15,
+# ggsave("plots/FLT3_ITD/ROC_test_top5_gbm_vst_min_max_3000.png", device = "png", width = 15, height = 15,
 #        units = "cm", pointsize = 10, dpi = 500)
 
-performance <- get_performance(top10_gbm_small[[5]], Fischer_vst_min_max, Fischer_labels,
-                               classes = c("FLT3_ITD.Mutant", "FLT3_ITD.WT"),
-                               plot_title = "Performance of gbm model 5 for FLT3 mutation on Fischer data",
-                               plot_subtitle=paste0("n.trees=", top10_gbm_small[[5]][["bestTune"]][["n.trees"]],
-                                                    " interaction.depth=", top10_gbm_small[[5]][["bestTune"]][["interaction.depth"]],
-                                                    " shrinkage=", top10_gbm_small[[5]][["bestTune"]][["shrinkage"]],
-                                                    " n.minobsinnode=", top10_gbm_small[[5]][["bestTune"]][["n.minobsinnode"]]))
+
+# Top 5 best performing gbms (Fischer data) ========================
+fischer_performance_grid <- gbm_all_performance %>% head(5) %>%
+  select(n.trees, interaction.depth, shrinkage, n.minobsinnode)
+fischer_performance_grid$Accuracy <- NA_real_
+fischer_performance_grid$Sens <- NA_real_
+fischer_performance_grid$Spec <- NA_real_
+fischer_performance_grid$Prec <- NA_real_
+fischer_performance_grid$Kappa <- NA_real_
+fischer_performance_grid$AUC <- NA_real_
+fischer_performance_top5_roc <- vector("list", nrow(fischer_performance_grid))
+
+for (i in seq_len(nrow(fischer_performance_grid))) {
+  
+  cat("Assessing performance of model", i, "of", nrow(fischer_performance_grid), "\n")
+  
+  model <- top10_gbm_small[[i]]
+  
+  performance_df <- get_performance(top10_gbm_small[[i]], Fischer_vst_min_max, Fischer_labels,
+                                    classes = c("FLT3_ITD.Mutant", "FLT3_ITD.WT"))
+  
+  # Extract metrics from confusion matrix
+  cm <- performance_df$confusion_matrix
+  fischer_performance_grid$Sens[i] <- cm$byClass["Sensitivity"]
+  fischer_performance_grid$Spec[i] <- cm$byClass["Specificity"]
+  fischer_performance_grid$Prec[i] <- cm$byClass["Pos Pred Value"]
+  fischer_performance_grid$Accuracy[i] <- cm$overall["Accuracy"]
+  fischer_performance_grid$Kappa[i] <- cm$overall["Kappa"]
+  fischer_performance_grid$AUC[i] <- performance_df$auc
+  
+  fischer_performance_top5_roc[[i]] <- performance_df$roc_df
+}
+
+names(fischer_performance_top5_roc) <- paste0("model ", seq_along(fischer_performance_top5_roc))
+fischer_performance_grid <- cbind(fischer_performance_grid, data.frame(model=paste0("model ", 1:5)))
+# write.table(fischer_performance_grid, "OutputTables/FLT3_ITD_tuned_top5_gbm_models_validation_Fischer_performance_table.txt",
+#             row.names = T, col.names = T, quote = F, sep = "\t")
+# saveRDS(fischer_performance_top5_roc, "OutputTables/FLT3_ITD_tuned_top5_gbm_models_validation_Fischer_performance_roc.rds")
+
+# Plot test ROC curves 
+roc_5_df <- bind_rows(lapply(1:5, function(i) {
+  df <- fischer_performance_top5_roc[[i]]
+  df$model <- paste0("Model ", i)
+  df
+}))
+auc_labels <- paste0("Model ", 1:5,
+                     " (AUC = ",
+                     round(fischer_performance_grid$AUC[1:5], 3), ")")
+roc_5_df$model <- factor(roc_5_df$model,
+                         levels = paste0("Model ", 1:5),
+                         labels = auc_labels)
+ggplot(roc_5_df, aes(x = fpr, y = tpr, color = model)) +
+  geom_line(size = 1.2) +
+  geom_abline(intercept = 0, slope = 1,
+              linetype = "dashed", color = "gray") +
+  labs(title = "ROC Curves for Top 5 gbm models on test data",
+       x = "False Positive Rate", y = "True Positive Rate",
+       color = "Model") +
+  theme_minimal() +
+  theme(panel.background = element_rect(fill = "white", color = NA),
+        plot.background  = element_rect(fill = "white", color = NA))
+# ggsave("plots/FLT3_ITD/ROC_Fischer_top5_gbm_vst_min_max_3000.png", device = "png", width = 15, height = 15,
+#        units = "cm", pointsize = 10, dpi = 500)
 
 
 # Check Variable importance ===========================
-varImp_df <- data.frame(variable=row.names(varImp(svm_tuned)$importance),
-                        importance=varImp(svm_tuned)$importance)
+varImp_df <- data.frame(variable=row.names(varImp(gbm_tuned_small)$importance),
+                        importance=varImp(gbm_tuned_small)$importance)
 varImp_top20 <- varImp_df %>%
-  arrange(desc(importance.NPM1.Mutant)) %>%
+  arrange(desc(Overall)) %>%
   head(20)
-ggplot(varImp_top20, aes(x = reorder(variable, importance.NPM1.Mutant), y = importance.NPM1.Mutant)) +
+ggplot(varImp_top20, aes(x = reorder(variable, Overall), y = Overall)) +
   geom_col(fill = "steelblue") +
   coord_flip() +
   labs(x = "Variable", y = "Importance",
-       title = "Top 20 most important genes for NPM1 mutation predicition (tuned SVM)")
-# ggsave("plots/NPM1/VariableImportance_plot_svm_tuned.png", device = "png",
+       title = "Top 20 most important genes for FLT3-ITD mutation predicition (tuned gbm)")
+# ggsave("plots/FLT3_ITD/VariableImportance_plot_gbm_tuned.png", device = "png",
 #        width = 18, height = 12, units = "cm", pointsize = 10, dpi = 500)
 
-
-
+varImp100 <- varImp_df %>% arrange(desc(Overall)) %>% head(100) %>% select(variable)
+# write.table(varImp100, "OutputTables/FLT3_ITD_varImp100.txt", quote = FALSE,
+#             row.names = FALSE, col.names = FALSE)
 
 
 
